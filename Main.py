4cf86bb22ff8e5d07e72d11fcc377f3ea1abd970
rafalosa <rafal.osad@gmail.com>
2021-04-05 17:37:07 +0200

    Handling some exceptions, merged files together, reconstructions will now be a Scan class methods. Added a option to load an external sinogram.

--- Main.py
+++ Main.py
@@ -1,13 +1,26 @@
 import sinograms
-import sinogram_reconstruction
 import matplotlib.pyplot as plt
-import seaborn as sb
 import numpy as np
+import matplotlib.image as mpimg
 
 if __name__ == '__main__':
 
-    im = sinograms.Scan("images/censor.png")
-    sinogram = im.generateSinogram()
-    _ = sinograms.showSinogram(sinogram)
-    sinogram_reconstruction.fourierReconstruction(sinogram)
+    img_path = "images/sample1.png"
+    image = mpimg.imread(img_path)
+    scan_obj = sinograms.Scan()
+    scan_obj.loadImage(img_path)
+    scan_obj.generateSinogram(101,100)
+    fourier,recon = scan_obj.fourierReconstruction()
+
+    fig,axs = plt.subplots(2,2)
+    axs[0,0].imshow(image)
+    axs[0,0].set_title('Original image')
+    axs[0,1].imshow(scan_obj.sinogram,cmap='gray')
+    axs[0, 1].set_title('Sinogram of the image')
+    axs[1,0].imshow(np.abs(fourier),cmap='hsv',vmin=-60,vmax=60)
+    axs[1, 0].set_title('FFT of the sinogram converted\nto polar coordinates (abs)')
+    axs[1,1].imshow(recon,cmap='gray')
+    axs[1, 1].set_title('Reconstructed image')
+    fig.tight_layout(pad=1.0)
+
     plt.show()
--- sinograms.py
+++ sinograms.py
@@ -1,17 +1,24 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import matplotlib.cm as cm
-import seaborn as sns
 import multiprocessing as mp
 from multiprocessing import shared_memory as sm
 from multiprocessing import Pool
 from functools import partial
+import scipy
+import scipy.interpolate
+from scipy import fft
+from scipy import ndimage
+
+"""
+Useful sources: http://bioeng2003.fc.ul.pt/Conference%20Files/papers/De%20Francesco,%20Fourier.pdf - English
+http://ncbj.edu.pl/zasoby/wyklady/ld_podst_fiz_med_nukl-01/med_nukl_10_v3.pdf - Polish
+
+"""
 
 
-def transformReferenceFramePoint(t:float,s:float,angle:float,x_offset:float,y_offset:float):
-    # Function that transforms points between the patient's reference frame and detector's reference frame,
-    # transformation consists of translation, rotation and back translation
+def transformReferenceFramePoint(t:float,s:float,angle:float,x_offset:float,y_offset:float) -> tuple:
+    '''Function that transforms points between the patient's reference frame and detector's reference frame,
+    transformation consists of translation, rotation and back translation'''
 
     x = (t-x_offset)*np.cos(angle) - (s-y_offset)*np.sin(angle)
     y = (t-x_offset)*np.sin(angle) + (s-y_offset)*np.cos(angle)
@@ -19,13 +26,8 @@ def transformReferenceFramePoint(t:float,s:float,angle:float,x_offset:float,y_of
     return x+x_offset,y+y_offset
 
 
-def showSinogram(sinogram_data):
-    fig, ax = plt.subplots()
-    ax = sns.heatmap(sinogram_data, cmap=cm.gray)
-    return fig,ax
-
-
-def calculateForDetectorPosition(lines_t_size, lines_s_size,image_shape,dtype,memory_block_name,angle):
+def calculateForDetectorPosition(lines_t_size:int, lines_s_size:int,image_shape:tuple,
+                                 dtype:type,memory_block_name:str,angle:float):
 
     width, height, _ = image_shape
     lines_t = np.linspace(0, width, lines_t_size)
@@ -58,30 +60,44 @@ def calculateForDetectorPosition(lines_t_size, lines_s_size,image_shape,dtype,me
 class Scan:
 
     # todo: The image should be square, so crop/pad the loaded image if it isn't square
-    def __init__(self,img_path:str):
-        self.image = mpimg.imread(img_path)
-        self.width = self.image.shape[0]
-        self.height = self.image.shape[1]
+    def __init__(self):
+        self.image = None
+        self.sinogram = None
+        self.width = None
+        self.height = None
 
-    def generateSinogram(self):
-        '''The patient's reference frame is stationary, detector's reference frame
+    def generateSinogram(self,resolution:int,path_resolution:int,processes:int = 0):
+
+        '''This function generates a sinogram for a given image. Parameters (resolution,path_resolution,processes)
+        resolution determines how many Xray beams are used to generate the sinogram as well as how many angular
+        positions are registered. path_resolution determines how many datapoints are summed in the process
+        of generating a sinogram for a given Xray. The bigger the values the more detailed sinogram will be generated
+        and the longer the computation time. processes dictates how many processes can be used to generate the sinogram.
+        If not specified, all but 2 CPU threads will remain unused by this process.'''
+
+        """The patient's reference frame is stationary, detector's reference frame
         rotates around the (0,0,0) point. Due to the way that matplotlib loads images (the image loads
         in the 1st quadrant of the coordinate system), translation transformation is first required before
-        rotating the reference frame'''
+        rotating the reference frame"""
 
         # Generating a sinogram can obviously be done better and quicker, this implementation represents the real
         # measurement process and is pretty slow.
 
-        resolution = 301
+        if self.image is None:
+            raise RuntimeError("Image required before generating a sinogram")
+
         number_of_rays = resolution  # How many X-ray beams/detector cells
-        path_resolution = 200  # How many data points for each beam/detector cell
         angle_resolution = resolution  # Angular step
         angles = np.linspace(np.pi / 2, np.pi * 3 / 2, angle_resolution + 1)
         image_memory_shared = sm.SharedMemory(create=True,size=self.image.nbytes)
         image_shared_copy = np.ndarray(self.image.shape,dtype=self.image.dtype,buffer=image_memory_shared.buf)
         image_shared_copy[:,:,:] = self.image[:,:,:]
 
-        CPU_S = mp.cpu_count() - 2
+        if processes:
+            CPU_S = processes
+        else:
+            CPU_S = mp.cpu_count() - 2
+
         pool = Pool(processes=CPU_S)
         function = partial(calculateForDetectorPosition,number_of_rays+1,path_resolution+1,
                            self.image.shape,self.image.dtype,image_memory_shared.name)
@@ -95,4 +111,59 @@ class Scan:
         image_memory_shared.close()
         image_memory_shared.unlink()
 
-        return sinogram
+        self.sinogram = sinogram
+
+    def loadSinogram(self,path:str):
+        sinogram = mpimg.imread(path)
+        sinogram_sum = np.sum(sinogram,axis=2)
+        self.sinogram = sinogram_sum
+
+    def loadImage(self,path:str):
+        self.image = mpimg.imread(path)
+
+    def fourierReconstruction(self) -> tuple:
+
+        # This reconstruction exploits the mathematical similarity of the definition of projection to 2D Fourier
+        # transform of absorption coefficient.
+
+        if self.sinogram is None:
+            raise RuntimeError("Generate or load a sinogram before attempting a reconstruction.")
+
+        fft_sinogram = scipy.fft.fftshift(scipy.fft(scipy.fft.ifftshift(self.sinogram, axes=1)), axes=1)
+        fourier_size = fft_sinogram.shape[0]
+        fourier_radius = len(fft_sinogram[0]) / 2
+        transformed_to_radial = []
+        ang_i = 0
+        angles = np.linspace(0, np.pi, fft_sinogram.shape[1])
+
+        for row in fft_sinogram:  # Converting cartesian to polar coordinates for calculated Fourier transform
+            for x in range(len(row)):
+                temp_x, temp_y = rotate(np.array([x - fourier_radius, 0]), angles[ang_i])
+                transformed_to_radial.append([temp_x, temp_y])
+            ang_i += 1
+
+        x_data = [data[0] + fourier_radius for data in transformed_to_radial]
+        y_data = [data[1] + fourier_radius for data in transformed_to_radial]
+
+        X, Y = np.meshgrid(np.arange(fourier_size), np.arange(fourier_size))  # Preparing coordinates for interpolation
+        X = X.flatten()
+        Y = Y.flatten()
+
+        interpolated_radial_fft = scipy.interpolate.griddata((x_data, y_data), fft_sinogram.flatten(), (X, Y),
+                                                             fill_value=0.0, method='cubic').reshape(
+            (fourier_size, fourier_size))
+
+        reconstruction = scipy.fft.fftshift(scipy.fft.ifft2(scipy.fft.ifftshift(interpolated_radial_fft)))
+
+        reconstruction = ndimage.rotate(np.abs(reconstruction), 180)
+
+        return interpolated_radial_fft, reconstruction
+
+
+def rotate(vector:np.ndarray,angle:float) -> np.ndarray:
+    '''Function rotates a given vector counterclockwise by a given angle in radians (vector,angle)'''
+
+    rot_matrix = [[np.cos(angle),-np.sin(angle)],[np.sin(angle),np.cos(angle)]]
+    return np.dot(rot_matrix,vector)
+
+
