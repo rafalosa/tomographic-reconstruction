29024e49c56236fda3b5a81b265e9c03596ab1df
rafalosa <rafal.osad@gmail.com>
2021-04-16 00:08:21 +0200

    Tidied up images

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import matplotlib.image as mpimg
 
 if __name__ == '__main__':
 
-    img_path = "images/sample5.png"
+    img_path = "images/sinograms/sample1.png"
     image = mpimg.imread(img_path)
     scan_obj = sinograms.Scan()
     #scan_obj.loadImage(img_path)
